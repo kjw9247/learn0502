a47bd54123ff95e02f23a7fc1f06eed77e471356
kjw9247 <kjw924747@gmail.com>
2025-05-08 16:09:44 +0900

    두개화면을가진웹앱

--- 02.해커뉴스/news-step2/app2.js
+++ 02.해커뉴스/news-step2/app2.js
@@ -1,8 +1,7 @@
 const xhr = new XMLHttpRequest()
-//제목을 클릭했을때 제목을 읽어와서 다른 화면에 출력하기
-const content = document.createElement("div")
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json"
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@ID.json"
+
 xhr.open("GET", NEWS_URL, false)
 xhr.send()
 
@@ -10,27 +9,34 @@ const newsList = JSON.parse(xhr.responseText)
 const ul = document.createElement("ul")
 
 window.addEventListener("hashchange",() => {
+    //console.log("The hash has changed!");
+    //console.log(location.hash);//#4878909
+    //#을 때어내고 뒤에 id값만 꺼내서 @id치환한다 
+    //0번째 자리에는 #이 들어있으니 1을 쓴다
     const id = location.hash.substring(1)
+    //console.log(id);
+    //반복되는 코드가 있다.
     xhr.open("GET", CONTENT_URL.replace("@ID", id), false)
     xhr.send()
-    const newsContent = JSON.parse(xhr.responseText)
+    const newsContent = JSON.parse(xhr.responseText)//NodeList
     console.log(newsContent);
-    const title = document.createElement("h1")
-    title.innerText = newsContent.title
-    content.appendChild(title)
-
-  });
+  },
+);
 
 
 for(let i=1; i<=10; i=i+1){
   const li = document.createElement("li")
+  //insert here
   const a = document.createElement("a")
+  //어떤 제목을 클릭 할 것인가는 사용자 마음이므로 정할 수 없다.
   a.href = `#${newsList[i].id}`
   a.innerText = `${newsList[i].title}(💮${newsList[i].comments_count})`
 
   li.appendChild(a)
-  ul.appendChild(li)
+  ul.appendChild(li) //ul태그 안에 li 태그 추가를 10번반복
 }
 
-const v_root = document.querySelector("#root").appendChild(ul) 
-document.querySelector("#root").appendChild(content) 
+const v_root = document.querySelector("#root").appendChild(ul)  
+//console.log(v_root.innerText);
+//v_root.innerHTML
+//`${}` 변수를 쓸수있다
--- 02.해커뉴스/news-step2/app3.js
+++ 02.해커뉴스/news-step2/app3.js
@@ -10,27 +10,44 @@ const newsList = JSON.parse(xhr.responseText)
 const ul = document.createElement("ul")
 
 window.addEventListener("hashchange",() => {
+    //console.log("The hash has changed!");
+    //console.log(location.hash);//#4878909
+    //#을 때어내고 뒤에 id값만 꺼내서 @id치환한다 
+    //0번째 자리에는 #이 들어있으니 1을 쓴다
     const id = location.hash.substring(1)
+    //console.log(id);
+    //반복되는 코드가 있다.
     xhr.open("GET", CONTENT_URL.replace("@ID", id), false)
     xhr.send()
-    const newsContent = JSON.parse(xhr.responseText)
+    const newsContent = JSON.parse(xhr.responseText)//NodeList
     console.log(newsContent);
-    const title = document.createElement("h1")
-    title.innerText = newsContent.title
+    //뉴스 제목을 h1태그 안에 작성
+    const title = document.createElement("h1")//<h1>
+    title.innerText = newsContent.title//<h1>뉴스제목<h1
+    console.log(title);
+    //<div id='root'></div>
     content.appendChild(title)
+      
 
-  });
+  });//end of hashchange
 
 
 for(let i=1; i<=10; i=i+1){
   const li = document.createElement("li")
+  //insert here
   const a = document.createElement("a")
+  //어떤 제목을 클릭 할 것인가는 사용자 마음이므로 정할 수 없다.
   a.href = `#${newsList[i].id}`
   a.innerText = `${newsList[i].title}(💮${newsList[i].comments_count})`
 
   li.appendChild(a)
-  ul.appendChild(li)
+  ul.appendChild(li) //ul태그 안에 li 태그 추가를 10번반복
 }
 
 const v_root = document.querySelector("#root").appendChild(ul) 
 document.querySelector("#root").appendChild(content) 
+
+
+//console.log(v_root.innerText);
+//v_root.innerHTML = "조회결과"
+//`${}` 변수를 쓸수있다
