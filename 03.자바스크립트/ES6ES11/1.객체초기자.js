eb0547489103f6e33c5e9ee7cdbfd42f5cc94fcb
kjw9247 <kjw924747@gmail.com>
2025-05-21 14:56:34 +0900

    5월 21일 깊은복사 및 얕은복사

---
+++ 03.자바스크립트/ES6ES11/1.객체초기자.js
@@ -0,0 +1,39 @@
+// 객체 초기자
+// 객체 초기자는 중괄호({})로 묶인 0개 이상의 객체의 프로퍼티명과 관련 값의 *쌍*을
+console.log('1');
+let i = 3
+console.log(i);
+{
+  // 컴파일을 하지 않는다 - 문법체크를 하지 않는다 - 컴파일 에러이다
+  // 변수 선언시 타입을 작성하지 않는다 - 타입을 알 수 없다
+  // 런타임시에 타입을 알 수 있다(결정된다)
+  const x = 1
+  console.log(x);
+
+  const emp = {
+    ename: 'scott',
+    job: 'CLERK',
+    sal : 5000,
+  }
+  const ename = 'tiger'
+  const job = 'SALESMAN'
+
+  const emp2 = {
+    ename: ename,
+    job: job
+  }
+  console.log(emp2);
+  
+  // key와 value가 동일 할 때는 생략이 가능
+
+  const emp3 = {
+    ename,
+    job,
+  }
+  console.log(emp3);
+}
+
+// 실행방법
+// node 1.객체초기자.js
+// nodemon 1.객체초기자.js
+// 
---
+++ 03.자바스크립트/ES6ES11/2.구조분해할당.js
@@ -0,0 +1,43 @@
+// Destructuring assignment
+// 구조분해 할당
+{
+  console.log(typeof {});
+  const sonata = {
+    carColor: 'black',
+    speed: 30,
+    wheelNum: 4
+  }
+  // 아래는 구조분해 할다을 사용하지 않았다
+  {
+    const carColor = sonata.carColor
+    const speed = sonata.speed
+    console.log(carColor, speed);
+  }
+  //
+  {
+    const { carColor, speed, wheelNum } = sonata
+    console.log(carColor, speed, wheelNum);
+
+    // 다른 이름으로 정의해서 사용 가능함
+    const { carColor: sonataColor, speed: sonataSpeed, wheelNum: sonataWheelNum } = sonata
+    console.log(sonataColor, sonataSpeed, sonataWheelNum);
+  }
+  // array 배열에서는 [] 기호를 사용한다
+  const fruits = ['🍒', '🍓' ,'🍅']
+  {
+  const one = fruits[0]
+  const two = fruits[0]
+  const three = fruits[0]
+  console.log(one, two, three);
+  }
+  // 구조 분행 할당으로 바꾸어 본다
+  {
+    const [one, two, three] = fruits
+    console.log(one, two, three);
+  }
+  // 객체 초기자 안에서만 접근이 가능하다
+  // 아래는 에러가 발생함
+  // console.log(one, two, three);
+}
+
+
